6acf2e444ea56dff9a6e8c21fa50a1cdce5c23cc
branikita <nikita-bragin@mail.ru>
2015-04-04 17:49:06 +0300

    custom styles

--- js/script.js
+++ js/script.js
@@ -1,15 +1,52 @@
 $(function () {
-    var $linkInput = $('<input>', {type: 'text'});
+    var $linkInput = $('<input>', {type: 'text'}),
+        $customStyle = $('<div>', {'class': 'editor__custom-style'});
 
     tinymce.init({
         plugins: [
-            "simple_link", "my_color", "my_color_bg", "fontfamily", "fontsize", "simplecontrols"
+            "simple_link", "my_color", "my_color_bg", "fontfamily", "fontsize", "simplecontrols", "custom_style"
         ],
         selector: ".js-editor",
         menubar: false,
         statusbar: false,
         skin: 'recorder',
         linkInput: $linkInput,
+        customStyle: $customStyle,
+        defaultStyles: [
+            {
+                title: 'Header1',
+                isDefault: true,
+                values: {
+                    fontFamily: 'Arial',
+                    fontSize: '12',
+                    bold: true,
+                    italic: true,
+                    backgroundColor: '#fff'
+                }
+            },
+            {
+                title: 'Header2',
+                isDefault: true,
+                values: {
+                    fontFamily: 'Arial',
+                    fontSize: '12',
+                    bold: true,
+                    italic: true,
+                    backgroundColor: 'blue',
+                    color: 'red'
+                }
+            },
+            {
+                title: 'Header3',
+                isDefault: true,
+                values: {
+                    fontFamily: 'Arial',
+                    fontSize: '36',
+                    bold: true,
+                    backgroundColor: '#fff'
+                }
+            }
+        ],
         toolbar: "fontfamily fontsize | bold italic underline | forecolor | backcolor | simpleLink",
         setup: tinyMceSetupHandler
     });
@@ -28,6 +65,7 @@ $(function () {
         $header.append($('<a>', {text: 'X', title: 'Close', href: '#'}));
         $toolbar.prepend($header);
         $toolbar.append($linkInput);
+        $toolbar.append($customStyle);
     }
 
     function editorFocusHandler() {
---
+++ js/vendors/tinymce/js/tinymce/plugins/custom_style/plugin.js
@@ -0,0 +1,153 @@
+tinymce.PluginManager.add('custom_style', function (editor) {
+    var defaultStyles = editor.settings.defaultStyles,
+        $textField = editor.settings.customStyle,
+        CustomStyle = function ($wrapper, styleName, styles, isDefault) {
+            this.$wrapper = $wrapper;
+            this.$el = null;
+            this.styleName = null;
+            this.isDefault = false;
+            this.styles = {
+                fontFamily: null,
+                fontSize: null,
+                bold: false,
+                italic: false,
+                underline: false,
+                backgroundColor: null,
+                color: null,
+                opacity: null
+            };
+
+            this.setDefault(isDefault);
+            this.setCurrentStyles(styles);
+            this.setStyleName(styleName);
+            this.createDomElement();
+            this.addListeners();
+        };
+
+    CustomStyle.prototype.setDefault = function (isDefault) {
+        this.isDefault = !!isDefault;
+    };
+
+    CustomStyle.prototype.isCurrent = function (styles) {
+        var isCurrentStyle = false,
+            that = this;
+
+        $.each(styles, function (styleName, value) {
+            if (that.styles[styleName] === value) {
+                isCurrentStyle = true;
+            } else {
+                isCurrentStyle = false;
+                return false;
+            }
+        });
+
+        return false;
+    };
+
+    CustomStyle.prototype.setCurrentStyles = function (styles) {
+        var thisStyles = this.styles;
+
+        $.each(thisStyles, function (styleName) {
+            var newStyle = styles[styleName];
+
+            if (newStyle) {
+                thisStyles[styleName] = newStyle;
+            }
+        });
+    };
+
+    CustomStyle.prototype.createDomElement = function () {
+        var $el,
+            styleName = this.styleName,
+            $wrapper = this.$wrapper;
+
+        $el = this.$el = $('<li>', {text: styleName});
+        this.applyStylesToTitle();
+        $wrapper.append($el);
+    };
+
+    CustomStyle.prototype.applyStylesToTitle = function () {
+        var css = {},
+            styles = this.styles;
+
+        $.each(styles, function (key, value) {
+            if (value === null) {
+                return;
+            }
+
+            if (value === false) {
+                return;
+            }
+
+            if (key === 'fontFamily') {
+                css['font-family'] = value;
+                return;
+            }
+
+            if (key === 'fontSize') {
+                css['font-size'] = value + 'px';
+                return;
+            }
+
+            if (key === 'bold') {
+                css['font-weight'] = 'bold';
+                return;
+            }
+
+            if (key === 'italic') {
+                css['font-style'] = 'italic';
+                return;
+            }
+
+            if (key === 'underline') {
+                css['text-decoration'] = 'underline';
+                return;
+            }
+
+            if (key === 'backgroundColor') {
+                css['background-color'] = value;
+                return;
+            }
+
+            if (key === 'color') {
+                css['color'] = value;
+                return;
+            }
+
+            if (key === 'opacity') {
+                css['opacity'] = value;
+            }
+        });
+
+        this.$el.css(css);
+    };
+
+    CustomStyle.prototype.applyStylesToEditor = function () {
+
+    };
+
+    CustomStyle.prototype.setStyleName = function (styleName) {
+        this.styleName = styleName || 'untitled';
+    };
+
+    CustomStyle.prototype.addListeners = function () {
+        this.$el.on('click', this.styleClickHandler.bind(this));
+    };
+
+    CustomStyle.prototype.styleClickHandler = function () {
+        debugger;
+    };
+
+    function setDropDownView($wrapper, defaultStyles) {
+        $.each(defaultStyles, function (index, style) {
+            var styleName = style.title,
+                isDefault = style.isDefault,
+                styleValues = style.values;
+
+            new CustomStyle($wrapper, styleName, styleValues, isDefault);
+        });
+    }
+
+
+    setDropDownView($textField, defaultStyles);
+});
